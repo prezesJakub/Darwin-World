aa94d2e8a98de9169968cf5a91401bad4dd75895
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2024-12-14 16:39:05 +0100

    Initialized new project

---
+++ Darwin-World/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("application")
+    id("java")
+    id("org.openjfx.javafxplugin") version "0.0.13"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+javafx {
+    version = "21"
+    modules = listOf("javafx.base", "javafx.controls", "javafx.fxml", "javafx.graphics", "javafx.media", "javafx.swing", "javafx.web")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
